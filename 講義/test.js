b0bf1b7616da5813788cfc27fd0c7c8d39bff3e8
いりえ <n024476@stg.nada.ac.jp>
2026-03-19 22:46:18 +0900

    Add files via upload

---
+++ 講義/test.js
@@ -0,0 +1,23 @@
+a=6066**3+1084**5;
+console.log(a);
+a=1;
+b=0;
+
+s=0;
+a=0;
+coun=0;
+for (let aan=1;aan<1001;aan+=1){
+    if ((aan%2!=0)&&(aan%3!=0)&&(aan%5!=0)&&(aan%7!=0)){
+        coun+=1;
+        console.log(aan)
+
+    }
+
+
+    
+}
+console.log(coun);
+function aho(){
+    console.log("ざーこ")
+}
+aho()
